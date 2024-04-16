9e67cb8a9f82c94d00fb17214f1cb45858330166
melissachow0 <melychow@icloud.com>
2024-04-15 23:42:22 -0700

    catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,6 +15,10 @@ def get_catalog():
     """
     with db.engine.begin() as connection:
         green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
+        blue_potions = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+        red_potions = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
+
+
 
 
     return [
@@ -24,5 +28,19 @@ def get_catalog():
                 "quantity": green_potions,
                 "price": 46,
                 "potion_type": [0, 100 , 0 , 0],
+            },
+            {
+                "sku": "RED_POTION",
+                "name": "red potion",
+                "quantity": red_potions,
+                "price": 40,
+                "potion_type": [100, 0 , 0 , 0],
+            },
+            {
+                "sku": "BLUE_POTION",
+                "name": "blue potion",
+                "quantity": blue_potions,
+                "price": 45,
+                "potion_type": [0, 0 , 100 , 0],
             }
         ]
