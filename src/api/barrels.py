9e29d041d71c064f76c21ed4535711ec45703c56
melissachow0 <melychow@icloud.com>
2024-05-01 23:35:39 -0700

    should buy max of biggest barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -94,8 +94,16 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
         potion_capacity = connection.execute(sqlalchemy.text("SELECT potion_capacity FROM capacity")).scalar()
         potion_capacity = potion_capacity * 50
-        biggest_barrel = sorted_barrels[0].price * 4
+        
+        
+        big_barrel_quantity = 0
+        for barrel in sorted_barrels:
+            if barrel.ml_per_barrel == sorted_barrels[0].ml_per_barrel:
+                big_barrel_quantity += 1
+
+        biggest_barrel = sorted_barrels[0].price * big_barrel_quantity
         min_quantity = max(gold//biggest_barrel, 1)
+
 
         #this code should prioritize buying medium barrels in order to bring price down
 
