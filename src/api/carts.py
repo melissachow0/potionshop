45f7e18126f06b5eaf0a93a1122674e38688b21a
melissachow0 <melychow@icloud.com>
2024-05-04 15:57:56 -0700

    it's Arcanday

--- src/api/carts.py
+++ src/api/carts.py
@@ -12,7 +12,7 @@ router = APIRouter(
     tags=["cart"],
     dependencies=[Depends(auth.get_api_key)],
 )
-day = "Bloomday"
+day = "Arcanday"
 
 class search_sort_options(str, Enum):
     customer_name = "customer_name"
